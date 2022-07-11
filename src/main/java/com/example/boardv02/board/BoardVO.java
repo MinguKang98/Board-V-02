403519743a0e08d410fc4e2f9b1021460d2fa1db
mingu <alsrn7590@naver.com>
2022-07-11 15:56:17 +0900

    게시판/BoardVO.java 생성

---
+++ src/main/java/com/example/boardv02/board/BoardVO.java
@@ -0,0 +1,103 @@
+package com.example.boardv02.board;
+
+public class BoardVO {
+    private int boardId;
+    private String createdDate;
+    private String updatedDate;
+    private String user;
+    private String password;
+    private String title;
+    private String content;
+    private int visitCount;
+    private int commentCount;
+    private boolean fileExist;
+    private int categoryId;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(int visitCount) {
+        this.visitCount = visitCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public boolean isFileExist() {
+        return fileExist;
+    }
+
+    public void setFileExist(boolean fileExist) {
+        this.fileExist = fileExist;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}

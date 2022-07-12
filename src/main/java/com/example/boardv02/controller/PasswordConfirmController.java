5e2df1190460ecffdac368328daf826bfe1ee804
mingu <alsrn7590@naver.com>
2022-07-12 23:05:26 +0900

    비밀번호 확인/PasswordConfirmController 생성
    PasswordConfirmController.java 생성

---
+++ src/main/java/com/example/boardv02/controller/PasswordConfirmController.java
@@ -0,0 +1,46 @@
+package com.example.boardv02.controller;
+
+import com.example.boardv02.dao.BoardDAO;
+import com.example.boardv02.vo.BoardVO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PasswordConfirmController implements Controller {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BoardDAO boardDAO = new BoardDAO();
+
+        int boardId = Integer.parseInt(request.getParameter("boardId"));
+        String type = request.getParameter("type");
+        String inputPassword = request.getParameter("password");
+
+        BoardVO board = boardDAO.getBoardById(boardId);
+        String originPassword = board.getPassword();
+
+        if (inputPassword.equals(originPassword)) {
+            // 같으면 type 에 따라 다음 절차
+            if (type.equals("modify")) { // 게시판 수정
+                RequestDispatcher dispatcher = request.getRequestDispatcher(
+                        "/WEB-INF/board/modify.jsp?boardId=" + boardId);
+                dispatcher.forward(request, response);
+            }
+
+            if (type.equals("delete")) { // 게시판 삭제
+                response.sendRedirect("boardDelete?boardId=" + boardId);
+            }
+        }
+        else{
+            // 다르면 passwordCheck 로
+            request.setAttribute("confirm", "fail");
+            RequestDispatcher dispatcher = request.getRequestDispatcher(
+                    "/WEB-INF/board/passwordCheck.jsp?boardId=" + boardId + "&type=" + type);
+            dispatcher.forward(request, response);
+
+        }
+    }
+}

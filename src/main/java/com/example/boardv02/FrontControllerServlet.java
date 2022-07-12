d36d27a885cfe088d5ba4295371b82d621e45c58
mingu <alsrn7590@naver.com>
2022-07-12 23:32:21 +0900

    프론트 컨트롤러/boardDelete 처리 추가
    boardDelete 처리 추가

--- src/main/java/com/example/boardv02/FrontControllerServlet.java
+++ src/main/java/com/example/boardv02/FrontControllerServlet.java
@@ -23,6 +23,7 @@ public class FrontControllerServlet extends HttpServlet {
         controllerMap.put("/board/boardSave", new BoardSaveController());
         controllerMap.put("/board/passwordCheck.jsp", new PasswordCheckController());
         controllerMap.put("/board/passwordConfirm", new PasswordConfirmController());
+        controllerMap.put("/board/boardDelete", new BoardDeleteController());
     }
 
     @Override
